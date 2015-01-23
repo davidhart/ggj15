00d9714dfc758d9d3a9a129d666d883354b38176
David Hart <david.hart555@googlemail.com>
2015-01-23 23:47:30 +0000

    Added InputManager to main scene and silenced some of the debug spam

--- UnityProject/Assets/Code/Input/InputManager.cs
+++ UnityProject/Assets/Code/Input/InputManager.cs
@@ -136,6 +136,6 @@ public class InputManager : MonoBehaviour {
 		if( initalizedJoystickCount != Input.GetJoystickNames().Length)
 			RefreshDevices();
 			
-		print( activeDevice.name + " " + activeDevice.state );
+		//print( activeDevice.name + " " + activeDevice.state );
 	}
 }
