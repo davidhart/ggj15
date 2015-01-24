b1eb5085b7d92df3442b8e49dad7aa4409237b16
Richard Bangs <bangs@bossalien.com>
2015-01-24 00:33:41 +0000

    - Added Character - Tile definitions

---
+++ UnityProject/Assets/Code/Core/Character.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+	public static Character Instance { get; private set; }
+
+	public int Rotation { get; private set; }
+
+	public int X { get { return Mathf.FloorToInt( gameObject.transform.position.x ); } }
+	public int Z { get { return Mathf.FloorToInt( gameObject.transform.position.z ); } }
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void MoveForward()
+	{
+		gameObject.transform.position += gameObject.transform.rotation * new Vector3( 0.0f, 0.0f, 1.0f );
+	}
+
+	void MoveBackward()
+	{
+		gameObject.transform.position -= gameObject.transform.rotation * new Vector3( 0.0f, 0.0f, 1.0f );
+	}
+
+	void RotateLeft()
+	{
+		Rotation++;
+
+		UpdateRotation();
+	}
+
+	void RotateRight()
+	{
+		Rotation--;
+
+		UpdateRotation();
+	}
+
+	void UpdateRotation()
+	{
+		if( Rotation < 0 )
+			Rotation = 3;
+
+		if( Rotation > 3 )
+			Rotation = 0;
+
+		gameObject.transform.rotation = Quaternion.AngleAxis( Rotation * 90.0f, Vector3.up );
+	}
+
+	void Update()
+	{
+		if( Input.GetKeyDown( KeyCode.A ) )
+			RotateLeft();
+
+		if( Input.GetKeyDown( KeyCode.D ) )
+			RotateRight();
+
+		if( Input.GetKeyDown( KeyCode.W ) )
+			MoveForward();
+
+		if( Input.GetKeyDown( KeyCode.S ) )
+			MoveBackward();
+	}
+}
---
+++ UnityProject/Assets/Code/Tiles/TileDefinition.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum eTileType
+{
+	Floor,
+	Wall
+}
+
+[ExecuteInEditMode]
+public class TileDefinition : MonoBehaviour
+{
+	public eTileType Type;
+
+	public int X { get { return Mathf.FloorToInt( gameObject.transform.position.x ); } }
+	public int Z { get { return Mathf.FloorToInt( gameObject.transform.position.z ); } }
+
+	void SnapToGrid()
+	{
+		Vector3 newPosition = new Vector3( X, 0.0f, Z );
+
+		if( newPosition.x < 0 )
+			newPosition.x = 0;
+
+		if( newPosition.z < 0 )
+			newPosition.z = 0;
+
+		gameObject.transform.position = newPosition;
+	}
+
+	void Update()
+	{
+		if( !Application.isPlaying )
+			SnapToGrid();
+	}
+}
